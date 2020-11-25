d338888e6b01825f468ead5e44e3cf51304f6ef6
santiago-alvarez <santiagoelpapasitoalvarez@hotmail.com>
2020-11-25 15:44:36 -0500

    Álvarez es un crack

--- src/Utiles/mocks/preguntas.js
+++ src/Utiles/mocks/preguntas.js
@@ -117,45 +117,247 @@ export const preguntas =[{
 	respuesta:"San Antonio de Prado tenía 32 Juntas de Acción Comunal y 6 Juntas de vivienda Comunitaria",
 	opcion1:"San Antonio de Prado tenía 30 Juntas de Acción Comunal y 16 Juntas de vivienda Comunitaria",
 	opcion2:"San Antonio de Prado tenía 31 Juntas de Acción Comunal y 17 Juntas de vivienda Comunitaria",
-	opcion3: "San antonio de Prado tenía 32 Juntas de Acción Comunal y 6 Juntas de vivienda Comunitaria"
+	opcion3:"San antonio de Prado tenía 32 Juntas de Acción Comunal y 6 Juntas de vivienda Comunitaria"
 },{
 	id:15,
 	pregunta:"¿Cuál es la población de San Antonio de Prado al año 2014?",
 	categoria:0,
 	respuesta:"Según el Dane 102.203 habitantes",
 	opcion1:"Según el Dane 112.203 habitantes",
-	opcion2:"Según el Dane 114.203 habitantes",
-	opcion3: "Según el Dane 105.203 habitantes"
+	opcion2:"Según el Dane 114.203 habitantes"
+	opcion3:"Según el Dane 105.203 habitantes"
 },{
 	id:16,
-	pregunta:"¿En que año se fundo el caserio que dio origen a San Antonio de Prado?",
+	pregunta:"¿CuáL era el nombre del conquistador español que pisó por primera vez suelo pradeño?",
 	categoria:1,
-	respuesta:"1869",
-	opcion1:"1789",
-	opcion2:"1863",
-	opcion3: "1910"
+	respuesta:"Jerónimo luis Tejelo",
+	opcion1:"Pedro de Heredia",
+	opcion2:"Alonso de Ojeda",
+	opcion3: "Rodrigo Batidas"
 },{
 	id:17,
-	pregunta:"¿En que año se fundo el caserio que dio origen a San Antonio de Prado?",
+	pregunta:"¿Cuales fueron los educadores más recordados por los habitantes de San Antonio de Prado? en el siglo pasado?",
 	categoria:2,
-	respuesta:"1869",
-	opcion1:"1789",
-	opcion2:"1863",
-	opcion3: "1910"
+	respuesta:"Don Carlos Betancur y la señorita Celina Escobar",
+	opcion1:"Don Manuel J. Betancur y la señorita Celina Escobar",
+	opcion2:"Don Carlos Betancur y la señorita Herlen Vásquez",
+	opcion3: " Don Manuel J. Betancur y la señorita Herlen Vasquez"
 },{
 	id:18,
-	pregunta:"¿En que año se fundo el caserio que dio origen a San Antonio de Prado?",
+	pregunta:"¿Cómo se llamó antiguamente el sector bajo Pradito y la Manguala",
 	categoria:3,
-	respuesta:"1869",
-	opcion1:"1789",
-	opcion2:"1863",
-	opcion3: "1910"
+	respuesta:"Buga",
+	opcion1:"Valle",
+	opcion2:"Cali",
+	opcion3: "Matadero"
 },{
 	id:19,
-	pregunta:"¿En que año se fundo el caserio que dio origen a San Antonio de Prado?",
+	pregunta:"¿Cuál es el nombre del organismo de representación política directa de comunidad a nivel corregimental?",
 	categoria:4,
-	respuesta:"1869",
-	opcion1:"1789",
-	opcion2:"1863",
-	opcion3: "1910"
+	respuesta:"Junta Administradora Local",
+	opcion1:"Junta de Acción Comunal",
+	opcion2:"Consejo Comunal y Corregimental de Planeación",
+	opcion3: "Asocomunal"
+},{
+	id:20,
+	pregunta:"¿Cuál es la maxíma altura que hay en San Antonio de Prado?",
+	categoria:0,
+	respuesta:"El cerro del Padre Amaya  con 3.100 metros sobre el nivel del mar",
+	opcion1:"El cerro del Padre Amaya  con 3.300 metros sobre el nivel del mar",
+	opcion2:"El cerro del Padre Amaya  con 3.200 metros sobre el nivel del mar",
+	opcion3: "El cerro del Padre Amaya  con 3.000 metros sobre el nivel del mar"
+},{
+	id:21,
+	pregunta:"¿Cómo se llamo el priemr hospital que tuvo San Antonio de Prado, y en que año empezó a funcionar?",
+	categoria:1,
+	respuesta:"Hospital de Caridad San Rafael de Prado, en 1889",
+	opcion1:"Hospital de Caridad San Rafael, en 1880",
+	opcion2:"Hospital de Caridad de Prado, en 1885",
+	opcion3: "Hospital de Prado, en 1888"
+},{
+	id:22,
+	pregunta:"¿En que año llegarón los hermanos Lasallistas a San Antonio de Prado?",
+	categoria:2,
+	respuesta:"En 1939",
+	opcion1:"En 1938",
+	opcion2:"En 1937",
+	opcion3: "En 1936"
+},{
+	id:23,
+	pregunta:"¿Cuál eral nombre deln sacerdote más famoso de la primera mitad del siglo XX, que además tenía fama de milagroso?",
+	categoria:3,
+	respuesta:"Sacerdote Benjamín Aristizábal",
+	opcion1:"Sacerdote Gabriel Mesa",
+	opcion2:"Sacerdote Jaime Hurtado",
+	opcion3: "Sacerdote Rene Botero"
+},{
+	id:24,
+	pregunta:"¿Cuáles son la vías de acceso a San Antonio de Prado?",
+	categoria:0,
+	respuesta:"La vía tradicional que pasa por Itagüi y la vía que desemboca a la Estrella al Sur del Área Metropolitana",
+	opcion1:"La vía tradicional que pasa por Itagüi y la vía que desemboca a Belen",
+	opcion2:"La vía a Armenia y la vía que desemboca a la Estrella al Sur del Área Metropolitana",
+	opcion3: "La vía tradicional que pasa por Itagüi y la vía a Armenia"
+},{
+	id:25,
+	pregunta:"¿Cuál era el nombre del industrial Antiqueño que más donaciones en obras hizo en San Antonio de Prado",
+	categoria:1,
+	respuesta:"Diego Echavaría Misas",
+	opcion1:" Emilio Valderrama",
+	opcion2:"Honorio Rúa",
+	opcion3: "Ihnacio Bencur"
+},{
+	id:26,
+	pregunta:"¿Cuál es el nombre del más recordado músixo clásico en San Antonio de Prado?",
+	categoria:2,
+	respuesta:"Hernando Montoya Betancur",
+	opcion1:"Isidro Betancur Puerta",
+	opcion2:"Manuel J. Betancur",
+	opcion3: "Bernabe Ortiz"
+},{
+	id:27,
+	pregunta:"¿En que sitio de San Antonio de Prado y por qué  se perdian los caminantes, según la tradición oral?",
+	categoria:3,
+	respuesta:"En el bosque de ' El Silencio', por que un duende los embolataba",
+	opcion1:"En 'La Laguna' por que una bruja los hacia perder",
+	opcion2:"En el 'Alto del Barcino' por que un duende los embolatababa",
+	opcion3: "En el bosque de 'El encanto'', por que una bruja las embolataba"
+},{
+	id:28,
+	pregunta:"¿En qué año se formuló y para qué sirve el Plan de Desarrollo Local?",
+	categoria:4,
+	respuesta:"Se formuló en el año 2007 y sirve como instrumento para que entre la comunidad y el estado construyamos el futuro que soñamos.",
+	opcion1:"Se formuló en el año 2017 y sirve como instrumento para que la comunidad.",
+	opcion2:"Se formuló en el año 2019 y sirve como instrumento para el estado construya el futuro que sueña.",
+	opcion3: "Se formuló en el año 2013."
+},{
+	id:29,
+	pregunta:"¿Cuál es la principal fuente hidrográfica de San Antonio de Prado?",
+	categoria:0,
+	respuesta:"La quebrada Doña María",
+	opcion1:"La quebrada la Manguala",
+	opcion2:"La quebrada Cabuyala",
+	opcion3: "La quebrada La Limona"
+},{
+	id:30,
+	pregunta:"¿Cuál era el nombre del primer sacerdote que dirigió la parroquia de San Antonio de Prado?",
+	categoria:1,
+	respuesta:"Prebítero José Meza",
+	opcion1:"Lorenzo Salazar",
+	opcion2:"Rene Botero",
+	opcion3: "Jorge Humberto Monsalve"
+},{
+	id:31,
+	pregunta:"¿Cómo se llamó el famoso Escultur Antiqueño nacido en San Antonio de Prado?",
+	categoria:2,
+	respuesta:"José Horacio Betancur",
+	opcion1:"Mario ortis",
+	opcion2:"Guillermo León Betancur",
+	opcion3: "Antonio Betancur"
+},{
+	id:32,
+	pregunta:"¿Quién fue 'Quico Coco'en la primera mitad del siglo XX en San Antonio de Prado?",
+	categoria:3,
+	respuesta:"Era corista del templo parroquial, Por culpa del licor perdió su empleo al entonar la canción equivocada en una ceremonia litúrgica",
+	opcion1:"Era pianista del templo parroquial, Por culpa del licor perdió su empleo al entonar la canción equivocada en una ceremonia litúrgica",
+	opcion2:"Era sacritan del templo parroquial, Por culpa del licor perdió su empleo al tocar las campanas en una ceremonia litúrgica",
+	opcion3: "Era corista del templo parroquial, Por culpa del licor perdió su empleo al no llegar a trabajar"
+},{
+	id:33,
+	pregunta:"¿Cómo se llama el instrumento legarl que nos orienta sobre los usos del suelo permitidos en San Antonio de Prado?",
+	categoria:4,
+	respuesta:"Plan de Ordenamiento Territorial (Pot)",
+	opcion1:"Progrma de planeación y presupuesto participativo (PPL)",
+	opcion2:"Pland de Desarrollo Local (PDL)",
+	opcion3: "Plan de Gestión Integral de Residuos Sólidos-PGIRS"
+},{
+	id:34,
+	pregunta:"¿Cuál es el principal producto pecuario de San Antonio de Prado?",
+	categoria:0,
+	respuesta:"El cerdo",
+	opcion1:"El Novillo",
+	opcion2:"Los pollos",
+	opcion3: "El cerdo y el pollo"
+},{
+	id:35,
+	pregunta:"¿En qué época se inició la construcción del actual templo parroquial de San Antonio de Prado?",
+	categoria:1,
+	respuesta:"En la época en que fue párroco el sacerdote Carlos Gómez (entre 1934 y 1943)",
+	opcion1:"En la época en que fue párroco el sacerdote Benjamin Aristizabal (entre 1909 y 1934)",
+	opcion2:"En la época en que fue párroco el sacerdote Carlos Gómez (entre 1934 y 1943)",
+	opcion3: "Matadero"
+},{
+	id:36,
+	pregunta:"¿En qué año y qué mes circuló la primera edición del periódico el reflector",
+	categoria:2,
+	respuesta:"En Septiembre de 1982",
+	opcion1:"En Mayo de 1985",
+	opcion2:"En Enero de 1981",
+	opcion3: "En Noviembre de 1987"
+},{
+	id:37,
+	pregunta:"¿Cuál era el nombre de la más famosa catequista que tuvo San Antonio de Prado en el siglo XX?",
+	categoria:3,
+	respuesta:"Bernardita Garcés. Enseño el catecismo del padre Astete a varias generaciones de pradeños",
+	opcion1:"La Hermana Regina, Hacía morir de miedo a los niños que presentaban su examen de catequesis",
+	opcion2:"Blanca Betancur, con su vos encantaba a quien la escuchaba",
+	opcion3: "Herlen Vásquez, gran colaboradora de la parroquia de San Antonio de Prado"
+},{
+	id:38,
+	pregunta:"¿Qué función cumple la mesa ambiental de san Antonio de Prado?",
+	categoria:4,
+	respuesta:"Interactuar con la comunidad y el estado en la defensa y cuidado del medio ambiente",
+	opcion1:"Desarrollar actividades para que la comunidad conozca su entorno natural",
+	opcion2:"Determinar las normas y reglas sobre el manejo del medio ambiente",
+	opcion3: "Velar por el cuidado del medio ambiente"
+},{
+	id:39,
+	pregunta:"¿Cómo se divide porcentualmete el suelo de San Antonio de Prado?",
+	categoria:0,
+	respuesta:"92.2% suelo rural y 7.8% suelo urbano",
+	opcion1:"82.2% suelo rural y 77.8% suelo urbano",
+	opcion2:"87.2% suelo rural y 12.8% suelo urbano",
+	opcion3: "88.2% suelo rural y 11.8% suelo urbano"
+},{
+	id:40,
+	pregunta:"¿Cuántos habitantes tenía San Antonio de Prado en 1960",
+	categoria:1,
+	respuesta:"5.780",
+	opcion1:"15.780",
+	opcion2:"10.780",
+	opcion3: "20.780"
+},{
+	id:41,
+	pregunta:"¿Cuál era el nombre de la organización que realizó las recordadas semanas culturales en las décadas de 1970 y 1980?",
+	categoria:2,
+	respuesta:"El Centro Cultural",
+	opcion1:"La Casa Cultural",
+	opcion2:"Centro de Desarrollo Cultural",
+	opcion3: "Casa de la cultura Celina Escobar"
+},{
+	id:42,
+	pregunta:"¿Cuál era el nombre del más connotado 'Tinterillo', coplero, decimero y escritor de cartas de amor por enacargo, en San Antonio de Prado en el siglo pasado?",
+	categoria:3,
+	respuesta:"Ángel Montoya",
+	opcion1:"Rodrigo Atehortua",
+	opcion2:"Ernesto Ortiz",
+	opcion3: "Samuel Escobar"
+}
+,{
+	id:43,
+	pregunta:"¿Para qué sirve el Consejo Corregimental de Cultura?",
+	categoria:4,
+	respuesta:"Para que la comunidad proponga, direccione, gestione y fiscalice en coordinación con el estado, el desarrollo cultural de San Antonio de Prado",
+	opcion1:"Para que la comunidad proponga, direccione, gestione y fiscalice el desarrollo cultural de San Antonio de Prado",
+	opcion2:"Para que el estado, el desarrollo cultural de San Antonio de Prado",
+	opcion3: "Para que la comunidad proponga el desarrollo cultural de San Antonio de Prado"
+}
+,{
+	id:44,
+	pregunta:"¿Cuál era el nombre de la organización que realizó las recordadas semanas culturales en las décadas de 1970 y 1980?",
+	categoria:0,
+	respuesta:"El Centro Cultural",
+	opcion1:"La Casa Cultural",
+	opcion2:"Centro de Desarrollo Cultural",
+	opcion3: "Casa de la cultura Celina Escobar"
 }]
